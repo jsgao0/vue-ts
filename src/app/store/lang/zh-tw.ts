e31efb310084bbb6be107fcb0e2a7749aeb04531
KenFront <flamma0917@gmail.com>
2018-06-18 02:40:39 +0800

    形態檢查

--- src/app/store/lang/zh-tw.ts
+++ src/app/store/lang/zh-tw.ts
@@ -1,10 +1,19 @@
 import lang from '@/langs/zh-tw.json'
+
+interface State {
+  lang: {
+    [keys: string]: string | undefined
+  }
+}
+
 export default {
   state: {
     lang,
   },
   getters: {
-    text: (state: any): any => (text: string): string => state.lang[text],
+    text: (state: State): ((text: string) => string) => (
+      text: string,
+    ): string => state.lang[text] || '',
   },
   mutations: {},
   actions: {},
--- src/app/store/lazyTest/index.ts
+++ src/app/store/lazyTest/index.ts
@@ -1,9 +1,12 @@
+interface State {
+  lazyText: string
+}
 export default {
   state: {
     lazyText: '動態測試',
   },
   getters: {
-    dynamicText: (state: any): string => state.lazyText,
+    dynamicText: (state: State): string => state.lazyText,
   },
   mutations: {},
   actions: {},
